e0fc538028b8e9472dfb0493f26755dc09d69a65
lonelygusenica <88908783+lonelygusenica@users.noreply.github.com>
2024-10-04 23:06:49 +0300

    Update Task2.java

--- src/main/java/org/knit/lab1/Task2.java
+++ src/main/java/org/knit/lab1/Task2.java
@@ -1,37 +1,130 @@
 package org.knit.lab1;
 
-/***
- * Задача 2
- * Создайте класс SimpleUrl
- * глядя на URL ниже создайте как можно больше полей в классе SimpleUrl:
- * https://test.ru/test/1072/page.jsp?intParam=12345&doubleParam=3.14&textParameter=someText
- * добавьте геттеры и сеттеры для полей
- * Переопределите метод toString() для вывода информации о полях класса:
- * protocol = https
- * address = test.ru
- * domainZone = ru
- * siteName = test
- * webpageName = page.jsp
- * webPageExtention = jsp
- * ....
- * Распарсите данный URL на переменные, создайте экземпляр класса SimpleUrl и выведите на экран
- * Дополнительно
- * попробуйте с другими URL в сети, подумайте как можно сгруппировать значения.
- *
- */
 public class Task2 {
-    public void execute() {
+    private String protocol;
+    private String address;
+    private String domainZone;
+    private String siteName;
+    private String path;
+    private String webpageName;
+    private String webPageExtension;
+    private String queryString;
 
-        String urlValue = "https://test.ru/test/1072/page.jsp?intParam=12345&doubleParam=3.14&textParameter=someText";
+    public Task2(String url) {
+        parseUrl(url);
+    }
 
-        /* tips
-         * urlValue.split(":");
-         */
+    private void parseUrl(String url) {
+        try {
+            String[] protocolSplit = url.split("://");
+            this.protocol = protocolSplit[0];
 
-        //todo create class SimpleUrl()
-        // SimpleUrl url= new SimpleUrl();
-        // url.setAddress(address);
-        // url.setProtocol(protocol)
-        // System.out.println(url);
+            String[] addressAndPath = protocolSplit[1].split("/", 2);
+            String address = addressAndPath[0];
+            this.address = address;
+
+            String[] domainParts = address.split("\\.");
+            this.siteName = domainParts[0];
+            this.domainZone = domainParts[domainParts.length - 1];
+
+            if (addressAndPath.length > 1) {
+                String[] pathAndQuery = addressAndPath[1].split("\\?", 2);
+                this.path = pathAndQuery[0];
+
+                String[] pathParts = path.split("/");
+                String lastPart = pathParts[pathParts.length - 1];
+                String[] webpageParts = lastPart.split("\\.");
+                this.webpageName = webpageParts[0];
+                this.webPageExtension = webpageParts[1];
+
+                if (pathAndQuery.length > 1) {
+                    this.queryString = pathAndQuery[1];
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Ошибка при парсинге URL: " + e.getMessage());
+        }
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDomainZone() {
+        return domainZone;
+    }
+
+    public void setDomainZone(String domainZone) {
+        this.domainZone = domainZone;
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getWebpageName() {
+        return webpageName;
+    }
+
+    public void setWebpageName(String webpageName) {
+        this.webpageName = webpageName;
+    }
+
+    public String getWebPageExtension() {
+        return webPageExtension;
+    }
+
+    public void setWebPageExtension(String webPageExtension) {
+        this.webPageExtension = webPageExtension;
+    }
+
+    public String getQueryString() {
+        return queryString;
+    }
+
+    public void setQueryString(String queryString) {
+        this.queryString = queryString;
+    }
+
+    @Override
+    public String toString() {
+        return "protocol = " + protocol + "\n" +
+                "address = " + address + "\n" +
+                "domainZone = " + domainZone + "\n" +
+                "siteName = " + siteName + "\n" +
+                "path = " + path + "\n" +
+                "webpageName = " + webpageName + "\n" +
+                "webPageExtension = " + webPageExtension + "\n" +
+                "queryString = " + queryString;
+    }
+
+    public static void main(String[] args) {
+        String url = "https://test.ru/test/1072/page.jsp?intParam=12345&doubleParam=3.14&textParameter=someText";
+        Task2 simpleUrl = new Task2(url);
+        System.out.println(simpleUrl);
     }
 }
